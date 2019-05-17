fda86a0b8fa73ad4335169b27aa70a5889935408
Aurélie Mazet <aurelie.mazet@coding-academy.fr>
2019-05-17 21:38:02 +0200

    v2

---
+++ portfolio.js
@@ -0,0 +1,15 @@
+window.onclick= function(){
+
+    let  toast = document.getElementsByClassName("facehappy")[0];
+    var emojiface=document.getElementsByClassName("emojiface")[0];
+    var happyface=document.getElementsByClassName("facehappy")[0];
+
+    var tl = new TimelineMax({onComplete:appearElem});
+    TweenMax.to(document.getElementById("toolone"),5,{rotation:100,x:"5%",delay:3});
+    TweenMax.to(document.getElementById("tooltwo"),5,{rotation:130,x:"5%",delay:4});
+    TweenMax.to(document.getElementById("toolthree"),5,{rotation:160,x:"5%",delay:5});
+    TweenMax.to(document.getElementById("toolfour"),5,{rotation:190,x:"5%",delay:6});
+    
+    tl.to(document.getElementById("tuyau"), 5, {right:100, ease:Bounce.easeOut,});
+    console.log("happy")
+}
