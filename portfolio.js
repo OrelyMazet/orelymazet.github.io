f48d1263ee2ca2084c7537532ceff6fc7ede9279
Aurélie Mazet <aurelie.mazet@coding-academy.fr>
2019-05-21 16:28:29 +0200

    avec JS

--- portfolio.js
+++ portfolio.js
@@ -1,15 +1,15 @@
-/*window.onclick= function(){
+window.onclick= function(){
 
-    let  toast = document.getElementsByClassName("facehappy")[0];
-    var emojiface=document.getElementsByClassName("emojiface")[0];
-    var happyface=document.getElementsByClassName("facehappy")[0];
+    let  toast = document.getElementById("face")[0];
+    //var emojiface=document.getElementsByClassName("emojiface")[0];
+    //var happyface=document.getElementsByClassName("facehappy")[0];
 
-    var tl = new TimelineMax({onComplete:appearElem});
+    var tl = new TimelineMax({onclick});
     TweenMax.to(document.getElementById("toolone"),5,{rotation:100,x:"5%",delay:3});
     TweenMax.to(document.getElementById("tooltwo"),5,{rotation:130,x:"5%",delay:4});
     TweenMax.to(document.getElementById("toolthree"),5,{rotation:160,x:"5%",delay:5});
     TweenMax.to(document.getElementById("toolfour"),5,{rotation:190,x:"5%",delay:6});
     
-    tl.to(document.getElementById("tuyau"), 5, {right:100, ease:Bounce.easeOut,});
+    //tl.to(document.getElementById("tuyau"), 5, {right:100, ease:Bounce.easeOut,});
     console.log("happy");
-}*/
+}
