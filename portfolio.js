d1b11dfd0e87068d17e3503b88e249f6de57eb49
Aurélie Mazet <aurelie.mazet@coding-academy.fr>
2019-05-21 18:21:09 +0200

    lien href dasn la page

--- portfolio.js
+++ portfolio.js
@@ -1,15 +1,27 @@
 window.onclick= function(){
 
-    let  toast = document.getElementById("face")[0];
+    let  toast = document.getElementById("face");
     //var emojiface=document.getElementsByClassName("emojiface")[0];
     //var happyface=document.getElementsByClassName("facehappy")[0];
 
     var tl = new TimelineMax({onclick});
-    TweenMax.to(document.getElementById("toolone"),5,{rotation:100,x:"5%",delay:3});
-    TweenMax.to(document.getElementById("tooltwo"),5,{rotation:130,x:"5%",delay:4});
-    TweenMax.to(document.getElementById("toolthree"),5,{rotation:160,x:"5%",delay:5});
-    TweenMax.to(document.getElementById("toolfour"),5,{rotation:190,x:"5%",delay:6});
+    TweenMax.to(document.getElementById("toolone"),10,{rotation:100,x:"5%",delay:3});
+    TweenMax.to(document.getElementById("tooltwo"),10,{rotation:130,x:"5%",delay:4});
+    TweenMax.to(document.getElementById("toolthree"),10,{rotation:160,x:"5%",delay:5});
+    TweenMax.to(document.getElementById("toolfour"),10,{rotation:190,x:"5%",delay:6});
     
-    //tl.to(document.getElementById("tuyau"), 5, {right:100, ease:Bounce.easeOut,});
+    tl.to(document.getElementById("face"), 5, {right:300,ease:Bounce.easeOut,});
     console.log("happy");
 }
+
+    function maFunction() {
+     var elmnt = document.getElementById("milieu");
+        elmnt.scrollIntoView();
+        console.log("joyeux");
+  }
+  
+  function scdeFunction() {
+    var elmnt = document.getElementById("message");
+       elmnt.scrollIntoView();
+       console.log("joyeux");
+ }
